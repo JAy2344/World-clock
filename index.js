475940f178f7d2cce829583fb197e9bf6de1ef61
Ayanda Dlamini <Janicedoe745@gmail.com>
2025-09-02 11:12:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+function setdate() {
+  let date = document.querySelector("#current");
+  date.innerHTML = new Date();
+}
+setTimeout(setdate, 5000);
+function currentdate() {
+  let current = document.querySelector("#milliseconds");
+  current.innerHTML = new Date();
+}
+setInterval(currentdate);
+
+
+//Challenge 2
+let today = moment().format("dddd Do MMMM");
+let todayElement = document.querySelector("#now");
+todayElement.innerHTML = today;
+
+//Challenge 3
+let futureElement = document.querySelector("#future-date");
+let future_date = moment().add(10, `years`).format("dddd Do MMMM YYYY");
+futureElement.innerHTML = future_date;
